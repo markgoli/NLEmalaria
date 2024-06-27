0d702240cba3e9dd72a4057cca293b6d510ef450
ALABYEKKUBO SSUUBI BRIAN <atbrian19@gmail.com>
2024-06-27 20:37:29 +0300

    New changes

--- API_endpoint/malaria_captioning/captions/models.py
+++ API_endpoint/malaria_captioning/captions/models.py
@@ -1,24 +1,25 @@
 # # from django.db import models
 
-# # # Create your models here.
-# # # captions/model.py
-# # from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, GPT2Tokenizer
+# Create your models here.
+# captions/model.py
+from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, GPT2Tokenizer
 
-# # model_name = "BRIAN12682/microscopy-image-captioning"
-# # model = VisionEncoderDecoderModel.from_pretrained(model_name)
-# # feature_extractor = ViTFeatureExtractor.from_pretrained(model_name)
-# # tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+# model_name = "BRIAN12682/microscopy-image-captioning"
+model_name = "BRIAN12682/malaria-captioning-v2"
+model = VisionEncoderDecoderModel.from_pretrained(model_name)
+feature_extractor = ViTFeatureExtractor.from_pretrained(model_name)
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 
-# # def predict_caption(image_path, max_length=50):
-# #     from PIL import Image
-# #     image = Image.open(image_path)
-# #     if image.mode != "RGB":
-# #         image = image.convert("RGB")
+def predict_caption(image_path, max_length=50):
+    from PIL import Image
+    image = Image.open(image_path)
+    if image.mode != "RGB":
+        image = image.convert("RGB")
     
-# #     pixel_values = feature_extractor(images=image, return_tensors="pt").pixel_values
-# #     output_ids = model.generate(pixel_values, max_length=max_length)
-# #     caption = tokenizer.decode(output_ids[0], skip_special_tokens=True)
-# #     return caption
+    pixel_values = feature_extractor(images=image, return_tensors="pt").pixel_values
+    output_ids = model.generate(pixel_values, max_length=max_length)
+    caption = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+    return caption
 
 
 # # captions/model.py
@@ -51,28 +52,28 @@
 #     return caption
 
 
-######2222222
-# captions/model.py
-from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTokenizer
-import os
+# ######2222222
+# # captions/model.py
+# # from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTokenizer
+# # import os
 
-# Ensure the model is downloaded and reassembled
-from download_and_reassemble import download_and_reassemble_model
-download_and_reassemble_model()
+# # # Ensure the model is downloaded and reassembled
+# # from download_and_reassemble import download_and_reassemble_model
+# # download_and_reassemble_model()
 
-model_path = "vitgpt_model"  # Path where the model is saved after download
+# # model_path = "vitgpt_model"  # Path where the model is saved after download
 
-model = VisionEncoderDecoderModel.from_pretrained(model_path)
-feature_extractor = ViTFeatureExtractor.from_pretrained(model_path)
-tokenizer = AutoTokenizer.from_pretrained(model_path)
+# # model = VisionEncoderDecoderModel.from_pretrained(model_path)
+# # feature_extractor = ViTFeatureExtractor.from_pretrained(model_path)
+# # tokenizer = AutoTokenizer.from_pretrained(model_path)
 
-def predict_caption(image_path):
-    from PIL import Image
-    image = Image.open(image_path)
-    if image.mode != "RGB":
-        image = image.convert("RGB")
+# # def predict_caption(image_path):
+# #     from PIL import Image
+# #     image = Image.open(image_path)
+# #     if image.mode != "RGB":
+# #         image = image.convert("RGB")
 
-    pixel_values = feature_extractor(images=image, return_tensors="pt").pixel_values
-    output_ids = model.generate(pixel_values)
-    caption = tokenizer.decode(output_ids[0], skip_special_tokens=True)
-    return caption
+# #     pixel_values = feature_extractor(images=image, return_tensors="pt").pixel_values
+# #     output_ids = model.generate(pixel_values)
+# #     caption = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+# #     return caption
