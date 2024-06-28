a40ed8e781ee876f5dd1e50013fd4e25d2b5857d
ALABYEKKUBO SSUUBI BRIAN <atbrian19@gmail.com>
2024-06-28 12:21:26 +0300

    Model loading solution

--- API_endpoint/malaria_captioning/captions/models.py
+++ API_endpoint/malaria_captioning/captions/models.py
@@ -1,15 +1,47 @@
-# # from django.db import models
+# # # from django.db import models
 
-# Create your models here.
+# # Create your models here.
+# # captions/model.py
+# from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, GPT2Tokenizer, ViTImageProcessor
+
+# # model_name = "BRIAN12682/microscopy-image-captioning"
+# model_name = "BRIAN12682/malaria-captioning-v2"
+# model = VisionEncoderDecoderModel.from_pretrained(model_name)
+# #feature_extractor = ViTFeatureExtractor.from_pretrained(model_name)
+# image_processor = ViTImageProcessor.from_pretrained(model_name)
+# tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+
+# def predict_caption(image_path, max_length=50):
+#     from PIL import Image
+#     image = Image.open(image_path)
+#     if image.mode != "RGB":
+#         image = image.convert("RGB")
+    
+#     # pixel_values = feature_extractor(images=image, return_tensors="pt").pixel_values
+#     pixel_values = image_processor(images=image, return_tensors="pt").pixel_values
+#     output_ids = model.generate(pixel_values, max_length=max_length)
+#     caption = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+#     return caption
+
+
 # captions/model.py
-from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, GPT2Tokenizer, ViTImageProcessor
+# captions/model.py
 
-# model_name = "BRIAN12682/microscopy-image-captioning"
+from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTokenizer
+
 model_name = "BRIAN12682/malaria-captioning-v2"
-model = VisionEncoderDecoderModel.from_pretrained(model_name)
-#feature_extractor = ViTFeatureExtractor.from_pretrained(model_name)
-image_processor = ViTImageProcessor.from_pretrained(model_name)
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+model = None
+feature_extractor = None
+tokenizer = None
+
+def load_model():
+    global model, feature_extractor, tokenizer
+    if model is None:
+        model = VisionEncoderDecoderModel.from_pretrained(model_name)
+        feature_extractor = ViTFeatureExtractor.from_pretrained(model_name)
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+load_model()
 
 def predict_caption(image_path, max_length=50):
     from PIL import Image
@@ -17,8 +49,7 @@ def predict_caption(image_path, max_length=50):
     if image.mode != "RGB":
         image = image.convert("RGB")
     
-    # pixel_values = feature_extractor(images=image, return_tensors="pt").pixel_values
-    pixel_values = image_processor(images=image, return_tensors="pt").pixel_values
+    pixel_values = feature_extractor(images=image, return_tensors="pt").pixel_values
     output_ids = model.generate(pixel_values, max_length=max_length)
     caption = tokenizer.decode(output_ids[0], skip_special_tokens=True)
     return caption
--- API_endpoint/malaria_captioning/captions/views.py
+++ API_endpoint/malaria_captioning/captions/views.py
@@ -1,3 +1,36 @@
+# # captions/views.py
+
+# from django.shortcuts import render
+# from django.http import JsonResponse
+# from django.views.decorators.csrf import csrf_exempt
+# from django.core.files.storage import default_storage
+# from .models import predict_caption
+# import os
+
+# @csrf_exempt
+# def caption_image_api(request):
+#     if request.method == 'POST' and 'image' in request.FILES:
+#         image = request.FILES['image']
+#         image_path = default_storage.save(image.name, image)
+#         full_image_path = os.path.join(default_storage.location, image_path)
+        
+#         caption = predict_caption(full_image_path)
+        
+#         return JsonResponse({'caption': caption})
+#     else:
+#         return JsonResponse({'error': 'No image provided'}, status=400)
+
+# def caption_image_web(request):
+#     if request.method == 'POST' and 'image' in request.FILES:
+#         image = request.FILES['image']
+#         image_path = default_storage.save(image.name, image)
+#         full_image_path = os.path.join(default_storage.location, image_path)
+        
+#         caption = predict_caption(full_image_path)
+        
+#         return render(request, 'caption/caption_result.html', {'caption': caption, 'image_url': default_storage.url(image_path)})
+#     return render(request, 'caption/upload_image.html')
+
 # captions/views.py
 
 from django.shortcuts import render
@@ -6,16 +39,21 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.files.storage import default_storage
 from .models import predict_caption
 import os
+from django.core.cache import cache
+from asgiref.sync import sync_to_async
 
 @csrf_exempt
-def caption_image_api(request):
+async def caption_image_api(request):
     if request.method == 'POST' and 'image' in request.FILES:
         image = request.FILES['image']
-        image_path = default_storage.save(image.name, image)
+        image_path = await sync_to_async(default_storage.save)(image.name, image)
         full_image_path = os.path.join(default_storage.location, image_path)
-        
-        caption = predict_caption(full_image_path)
-        
+
+        caption = cache.get(full_image_path)
+        if not caption:
+            caption = await sync_to_async(predict_caption)(full_image_path)
+            cache.set(full_image_path, caption, timeout=60*15)  # Cache for 15 minutes
+
         return JsonResponse({'caption': caption})
     else:
         return JsonResponse({'error': 'No image provided'}, status=400)
@@ -25,8 +63,11 @@ def caption_image_web(request):
         image = request.FILES['image']
         image_path = default_storage.save(image.name, image)
         full_image_path = os.path.join(default_storage.location, image_path)
-        
-        caption = predict_caption(full_image_path)
-        
+
+        caption = cache.get(full_image_path)
+        if not caption:
+            caption = predict_caption(full_image_path)
+            cache.set(full_image_path, caption, timeout=60*15)  # Cache for 15 minutes
+
         return render(request, 'caption/caption_result.html', {'caption': caption, 'image_url': default_storage.url(image_path)})
     return render(request, 'caption/upload_image.html')
